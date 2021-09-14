306012556f516735fe8f4d0a5bc0e409d79f85fc
Arif-Hosen <aharifhosen1998@gmail.com>
2021-09-14 19:43:00 +0600

    first

--- js/app.js
+++ js/app.js
@@ -7,21 +7,24 @@ const loadProducts = () => {
 
 // show all product in UI 
 const showProducts = (products) => {
+  // console.log(products);
+
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src="${image}"></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button onclick="addToCart(${product.id} ,${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
+
     document.getElementById("all-products").appendChild(div);
   }
 };
@@ -29,7 +32,7 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
+  updateTotal();
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
@@ -75,6 +78,9 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
+  console.log(getInputValue("price"));
   document.getElementById("total").innerText = grandTotal;
 };
 loadProducts();
+
+
